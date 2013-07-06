2689efb8d9e7ba80eeea9fbaeccee034a317ddf2
Stefan Hagdahl <stefan.hagdahl83@gmail.com>
2013-07-06 21:21:12 +0200

    Add main code to game class

--- SPTetris2.0/header/Game.h
+++ SPTetris2.0/header/Game.h
@@ -1,14 +1,36 @@
 #ifndef GAME_H
 #define GAME_H
 
+#include <SFML/Graphics.hpp>
+#include "Block.h"
+#include "IPiece.h"
+#include "PieceBuilder.h"
+#include "Map.h"
+
 class Game
 {
 public:
 	Game();
 	~Game();
 
+	void loop(RenderWindow &window);
+
 private:
 
+	void handleinput();
+	void update();
+	void draw();
+
+
+	TetrisPiece *mCurrentPiece;
+	PieceBuilder *mPieceBuilder;
+	Map *mMap;
+	
+	
+	sf::Clock mClock;
+	float dt;
+	float maxFps;
+	PieceBuilder::Piece mPiece;
 };
 
 #endif
--- SPTetris2.0/source/Game.cpp
+++ SPTetris2.0/source/Game.cpp
@@ -8,10 +8,166 @@
 
 Game::Game()
 {
+	mCurrentPiece = NULL;
+	mPieceBuilder = new PieceBuilder();
 
+	mMap = new Map(20, 30);
+
+	dt = 0.0f;
+	maxFps = 1.0f / 30.0f;
 }
 
 Game::~Game()
+{
+
+}
+
+void Game::loop(RenderWindow &window)
+{
+	//temp stuff start
+	Vector2f start = mMap->getStartPos();
+	//temp stuff end
+	while (window.isOpen())
+    {
+		dt = mClock.restart().asMilliseconds() / 1000.0f;
+		dt = min(dt, maxFps);
+
+        Event event;
+        while (window.pollEvent(event))
+        {
+			switch(event.type)
+			{
+			case Event::Closed:
+				window.close();
+				break;
+
+			case Event::KeyPressed:
+				switch(event.key.code)
+				{
+				case Keyboard::Space:
+					if(mCurrentPiece != NULL)
+					{
+						mCurrentPiece->rotate();
+						if(!mPieceBuilder->isValidMove(*mCurrentPiece))
+						{
+							mCurrentPiece->revertMove();
+							mCurrentPiece->setOldRotationStage();
+						}
+						
+					}
+					break;
+				case Keyboard::C:
+					// Release the piece into invdiual blocks before creating a new one
+					// and drop it on to the map
+					if(mCurrentPiece != NULL)
+					{
+						mMap->drop(*mCurrentPiece);
+						mPieceBuilder->delPiece(mCurrentPiece);
+						mCurrentPiece = NULL;
+					}
+					// Create a new piece
+					mPiece = (PieceBuilder::Piece)(rand() % 7);
+					mCurrentPiece = &mPieceBuilder->addPiece(mPiece, start.x, start.y);
+					break;
+				case Keyboard::A:
+					// Move Piece to the left
+					if(mCurrentPiece != NULL)
+					{
+						mCurrentPiece->move(TetrisPiece::LEFT);
+						if(!mPieceBuilder->isValidMove(*mCurrentPiece))
+							mCurrentPiece->revertMove();
+						if(!mMap->isValidMove(*mCurrentPiece))
+							mCurrentPiece->revertMove();
+					}
+					break;
+				case Keyboard::D:
+					// Move Piece to the right
+					if(mCurrentPiece != NULL)
+					{
+						mCurrentPiece->move(TetrisPiece::RIGHT);
+						if(!mPieceBuilder->isValidMove(*mCurrentPiece))
+							mCurrentPiece->revertMove();
+						if(!mMap->isValidMove(*mCurrentPiece))
+							mCurrentPiece->revertMove();
+					}
+					break;
+				default:
+					break;
+				}
+			default:
+				break;
+			}
+
+        }
+
+		// Update Piece falling
+		if(mCurrentPiece != NULL)
+		{
+			mCurrentPiece->fall(dt);
+			// Is the piece colliding with another block?
+			if(!mPieceBuilder->isValidMove(*mCurrentPiece))
+			{
+				// Move back to old position and sync with map
+				mCurrentPiece->revertMove();
+				mMap->syncPiece(*mCurrentPiece);
+				// Move piece one tile at a time until collision
+				while(mPieceBuilder->isValidMove(*mCurrentPiece))
+				{
+					mCurrentPiece->move(TetrisPiece::DOWN);
+				}
+				// use old pos
+				mCurrentPiece->revertMove();
+				// Release the piece into invdiual blocks before creating a new one
+				// and drop it on to the map
+				mMap->drop(*mCurrentPiece);
+				mPieceBuilder->delPiece(mCurrentPiece);
+				mCurrentPiece = NULL;
+
+				// Map should check if rows have be come complete
+			}
+			// Is the piece below the map?
+			else if(!mMap->isValidMove(*mCurrentPiece))
+			{
+				// Move back to last position and sync with map
+				mCurrentPiece->revertMove();
+				mMap->syncPiece(*mCurrentPiece);
+				// move piece one tile at a time until last outside map then
+				while(mMap->isValidMove(*mCurrentPiece))
+				{
+					mCurrentPiece->move(TetrisPiece::DOWN);
+				}
+				// use old pos
+				mCurrentPiece->revertMove();
+				// Release the piece into invdiual blocks before creating a new one
+				// and drop it on to the map
+				mMap->drop(*mCurrentPiece);
+				mPieceBuilder->delPiece(mCurrentPiece);
+				mCurrentPiece = NULL;
+				
+			}
+
+		}
+		window.clear(Color::Black);
+		//Draw map first
+		mMap->draw(&window);
+		if(mCurrentPiece)
+		{
+			mCurrentPiece->draw(&window);
+		}
+		mPieceBuilder->draw(&window);
+		//b->draw(&window);
+		//window.draw(*b);
+
+		window.display();
+    }
+}
+
+void Game::handleinput()
+{
+
+}
+
+void Game::draw()
 {
 
 }
