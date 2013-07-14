f4f82b1ae155a880ad35376b406f8b969525c306
Stefan Hagdahl <stefan.hagdahl83@gmail.com>
2013-07-14 19:44:06 +0200

    Add Tetris include file

---
+++ SPTetris2.0/header/Tetris.h
@@ -0,0 +1,16 @@
+#ifndef TETRIS_H
+#define TETRIS_H
+
+enum GameState
+{
+	Paused     = 0,
+	Playing    = 1,
+	Clearing   = 2,
+	Dropping   = 3,
+	Meny       = 4,
+	New        = 5,
+	Quit       = 6
+};
+
+
+#endif
